5890a1ea0d4c674ef7370bc307a171efdae253be
RubinhoCR7 <99280508+RubinhoCR7@users.noreply.github.com>
2023-01-24 20:07:48 +0100

    Exo 5

---
+++ ex05:/ft_print_comb.js
@@ -0,0 +1,7 @@
+for (let i = 001; i <= 999; i++) {
+      for (let j = i + 1; j <= 9; j++) {
+        for (let k = j + 1; k <= 9; k++) {
+          console.log(i * 100 + j * 10 + k);
+        }
+      }
+    }
